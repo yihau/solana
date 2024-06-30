d864d50382cc977ac6885d93984fb11b010302ba
yihau <yihau.chen@icloud.com>
2024-07-01 02:54:14 +0800

    XXX: ci testing

--- ci/docker-run-default-image.sh
+++ ci/docker-run-default-image.sh
@@ -7,4 +7,5 @@ here="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1091
 source "$here/rust-version.sh"
 
+ci_docker_image=anzaxyz/ci:rust_1.78.0_nightly-2024-05-02_rpbf_fuzz
 "$here/docker-run.sh" "${ci_docker_image:?}" "$@"
