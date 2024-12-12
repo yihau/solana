35bb696c341145bb17baec73ea9a9a60daa6b171
yihau <yihau.chen@icloud.com>
2024-12-13 00:53:32 +0800

    add set public address test

--- validator/src/commands/set_public_address/mod.rs
+++ validator/src/commands/set_public_address/mod.rs
@@ -4,6 +4,31 @@ use {
     std::{net::SocketAddr, path::PathBuf, process::exit},
 };
 
+pub struct SetPublicAddressArg {
+    pub tpu_addr: Option<SocketAddr>,
+    pub tpu_forwards_addr: Option<SocketAddr>,
+}
+
+impl SetPublicAddressArg {
+    pub fn new(matches: &ArgMatches) -> Self {
+        let parse_arg_addr = |arg_name: &str, arg_long: &str| -> Option<SocketAddr> {
+            matches.value_of(arg_name).map(|host_port| {
+                solana_net_utils::parse_host_port(host_port).unwrap_or_else(|err| {
+                    eprintln!(
+                        "Failed to parse --{arg_long} address. It must be in the HOST:PORT \
+                     format. {err}"
+                    );
+                    exit(1);
+                })
+            })
+        };
+        SetPublicAddressArg {
+            tpu_addr: parse_arg_addr("tpu_addr", "tpu"),
+            tpu_forwards_addr: parse_arg_addr("tpu_forwards_addr", "tpu-forwards"),
+        }
+    }
+}
+
 pub fn command<'a>(_default_args: &'a DefaultArgs) -> App<'a, 'a> {
     SubCommand::with_name("set-public-address")
         .about("Specify addresses to advertise in gossip")
@@ -33,19 +58,7 @@ pub fn command<'a>(_default_args: &'a DefaultArgs) -> App<'a, 'a> {
 }
 
 pub fn execute(matches: &ArgMatches, ledger_path: &PathBuf) {
-    let parse_arg_addr = |arg_name: &str, arg_long: &str| -> Option<SocketAddr> {
-        matches.value_of(arg_name).map(|host_port| {
-            solana_net_utils::parse_host_port(host_port).unwrap_or_else(|err| {
-                eprintln!(
-                    "Failed to parse --{arg_long} address. It must be in the HOST:PORT \
-                 format. {err}"
-                );
-                exit(1);
-            })
-        })
-    };
-    let tpu_addr = parse_arg_addr("tpu_addr", "tpu");
-    let tpu_forwards_addr = parse_arg_addr("tpu_forwards_addr", "tpu-forwards");
+    let set_public_address_arg = SetPublicAddressArg::new(matches);
 
     macro_rules! set_public_address {
         ($public_addr:expr, $set_public_address:ident, $request:literal) => {
@@ -62,10 +75,39 @@ pub fn execute(matches: &ArgMatches, ledger_path: &PathBuf) {
             }
         };
     }
-    set_public_address!(tpu_addr, set_public_tpu_address, "setPublicTpuAddress");
     set_public_address!(
-        tpu_forwards_addr,
+        set_public_address_arg.tpu_addr,
+        set_public_tpu_address,
+        "setPublicTpuAddress"
+    );
+    set_public_address!(
+        set_public_address_arg.tpu_forwards_addr,
         set_public_tpu_forwards_address,
         "setPublicTpuForwardsAddress"
     );
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, std::net::SocketAddr};
+
+    #[test]
+    fn test_set_public_address_arg() {
+        let default_args = DefaultArgs::default();
+        let app = command(&default_args);
+        let matches = app.get_matches_from(vec![
+            "set-public-address",
+            "--tpu",
+            "127.0.0.1:8080",
+            "--tpu-forwards",
+            "127.0.0.1:8081",
+        ]);
+
+        let arg = SetPublicAddressArg::new(&matches);
+        assert_eq!(arg.tpu_addr, Some(SocketAddr::from(([127, 0, 0, 1], 8080))));
+        assert_eq!(
+            arg.tpu_forwards_addr,
+            Some(SocketAddr::from(([127, 0, 0, 1], 8081)))
+        );
+    }
 }
