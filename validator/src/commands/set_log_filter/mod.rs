6127b3933f53d0a001ec81c3f91ecec7b184d60d
yihau <yihau.chen@icloud.com>
2025-02-15 01:39:47 +0800

    SetLogFilterArg -> SetLogFilterArgs

--- validator/src/commands/set_log_filter/mod.rs
+++ validator/src/commands/set_log_filter/mod.rs
@@ -7,13 +7,13 @@ use {
 const COMMAND: &str = "set-log-filter";
 
 #[derive(Debug, PartialEq)]
-pub struct SetLogFilterArg {
+pub struct SetLogFilterArgs {
     pub filter: Option<String>,
 }
 
-impl FromClapArgMatches for SetLogFilterArg {
+impl FromClapArgMatches for SetLogFilterArgs {
     fn from_clap_arg_match(matches: &ArgMatches) -> Self {
-        SetLogFilterArg {
+        SetLogFilterArgs {
             filter: matches.value_of("filter").map(|s| s.to_string()),
         }
     }
@@ -32,12 +32,12 @@ pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
 }
 
 pub fn execute(matches: &ArgMatches, ledger_path: &Path) {
-    let set_log_filter_arg = SetLogFilterArg::from_clap_arg_match(matches);
-    if set_log_filter_arg.filter.is_none() {
+    let set_log_filter_args = SetLogFilterArgs::from_clap_arg_match(matches);
+    if set_log_filter_args.filter.is_none() {
         eprintln!("filter is required");
         exit(1);
     }
-    let filter = set_log_filter_arg.filter.unwrap();
+    let filter = set_log_filter_args.filter.unwrap();
 
     let admin_client = admin_rpc_service::connect(ledger_path);
     admin_rpc_service::runtime()
@@ -57,7 +57,7 @@ mod tests {
         verify_args_struct_by_command(
             command(&DefaultArgs::default()),
             vec![COMMAND],
-            SetLogFilterArg { filter: None },
+            SetLogFilterArgs { filter: None },
         );
     }
 
@@ -66,7 +66,7 @@ mod tests {
         verify_args_struct_by_command(
             command(&DefaultArgs::default()),
             vec![COMMAND, "expected_filter_value"],
-            SetLogFilterArg {
+            SetLogFilterArgs {
                 filter: Some("expected_filter_value".to_string()),
             },
         );
