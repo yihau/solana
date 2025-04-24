a707d03bc3d45ff651cb26a1051e5e7fc62cfbdd
yihau <yihau.chen@icloud.com>
2025-04-24 13:49:18 +0800

    clippy: allow result_large_err

--- scripts/cargo-clippy-nightly.sh
+++ scripts/cargo-clippy-nightly.sh
@@ -29,4 +29,5 @@ source "$here/../ci/rust-version.sh" nightly
   --deny=clippy::default_trait_access \
   --deny=clippy::arithmetic_side_effects \
   --deny=clippy::manual_let_else \
-  --deny=clippy::used_underscore_binding
+  --deny=clippy::used_underscore_binding \
+  --allow=clippy::result_large_err
