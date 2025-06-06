bfc00e8693ee90c86360e18b67d2d8a420786f97
yihau <yihau.chen@icloud.com>
2025-06-06 13:11:15 +0800

    decouple docs build from main ci docker image

--- docs/build.sh
+++ docs/build.sh
@@ -8,8 +8,8 @@ source ../ci/env.sh
 
 # shellcheck source=ci/rust-version.sh
 source ../ci/rust-version.sh
-../ci/docker-run-default-image.sh docs/build-cli-usage.sh
-../ci/docker-run-default-image.sh docs/convert-ascii-to-svg.sh
+./build-cli-usage.sh
+./convert-ascii-to-svg.sh
 ./set-solana-release-tag.sh
 
 # Build from /src into /build
