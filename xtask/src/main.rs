c4f8bf1787c01127919aa3577e1b31d8374edba0
yihau <yihau.chen@icloud.com>
2025-06-27 19:58:36 +0800

    init

---
+++ xtask/src/main.rs
@@ -0,0 +1,67 @@
+use {
+    anyhow::Result,
+    clap::{Args, Parser, Subcommand},
+    log::{error, info},
+};
+
+mod commands;
+mod common;
+
+#[derive(Parser)]
+#[command(name = "xtask", about = "Custom build tasks", version)]
+struct Xtask {
+    #[command(flatten)]
+    pub global: GlobalOptions,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(about = "Hello")]
+    Hello,
+    #[command(about = "Bump version")]
+    BumpVersion(commands::bump_version::BumpArgs),
+}
+
+#[derive(Args, Debug)]
+pub struct GlobalOptions {
+    /// Enable verbose (debug) logging
+    #[arg(short, long, global = true)]
+    pub verbose: bool,
+}
+
+fn main() {
+    if let Err(err) = try_main() {
+        error!("Error: {err}");
+        for (i, cause) in err.chain().skip(1).enumerate() {
+            error!("  {}: {}", i.saturating_add(1), cause);
+        }
+        std::process::exit(1);
+    }
+}
+
+fn try_main() -> Result<()> {
+    // parse the command line arguments
+    let xtask = Xtask::parse();
+
+    // set the log level
+    if xtask.global.verbose {
+        std::env::set_var("RUST_LOG", "debug");
+    } else {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+
+    // run the command
+    match xtask.command {
+        Commands::Hello => {
+            info!("Hello!");
+        }
+        Commands::BumpVersion(args) => {
+            commands::bump_version::run(args)?;
+        }
+    }
+    Ok(())
+}
