2883484058545b629dd602032fc3c03beda3fcd2
yihau <yihau.chen@icloud.com>
2025-07-11 11:51:42 +0800

    --enable-rpc-transaction-history

--- validator/src/commands/run/args/json_rpc_config.rs
+++ validator/src/commands/run/args/json_rpc_config.rs
@@ -7,7 +7,37 @@ use {
 impl FromClapArgMatches for JsonRpcConfig {
     fn from_clap_arg_match(matches: &ArgMatches) -> Result<Self> {
         Ok(JsonRpcConfig {
+            enable_rpc_transaction_history: matches.is_present("enable_rpc_transaction_history"),
             ..Default::default()
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::commands::run::args::{
+            tests::verify_args_struct_by_command_run_with_identity_setup, RunArgs,
+        },
+    };
+
+    #[test]
+    fn verify_args_struct_by_command_run_with_enable_rpc_transaction_history() {
+        {
+            let default_run_args = crate::commands::run::args::RunArgs::default();
+            let expected_args = RunArgs {
+                json_rpc_config: JsonRpcConfig {
+                    enable_rpc_transaction_history: true,
+                    ..default_run_args.json_rpc_config.clone()
+                },
+                ..default_run_args.clone()
+            };
+            verify_args_struct_by_command_run_with_identity_setup(
+                default_run_args,
+                vec!["--enable-rpc-transaction-history"],
+                expected_args,
+            );
+        }
     }
 }
--- validator/src/commands/run/execute.rs
+++ validator/src/commands/run/execute.rs
@@ -547,7 +547,7 @@ pub fn execute(
         expected_shred_version,
         new_hard_forks: hardforks_of(matches, "hard_forks"),
         rpc_config: JsonRpcConfig {
-            enable_rpc_transaction_history: matches.is_present("enable_rpc_transaction_history"),
+            enable_rpc_transaction_history: run_args.json_rpc_config.enable_rpc_transaction_history,
             enable_extended_tx_metadata_storage: matches
                 .is_present("enable_extended_tx_metadata_storage"),
             rpc_bigtable_config,
