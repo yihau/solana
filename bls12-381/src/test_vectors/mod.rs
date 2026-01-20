4827ead2de584aaead5ca93e4d09baf5dbb3e92a
Sam Kim <skim13@cs.stanford.edu>
2026-01-20 22:07:35 +0900

    [bls12-381] Add `agave-bls12-381` crate for SIMD-388 (#9759)
    * add `agave-bls12-381` crate for SIMD-388
    
    * fix `{G1Affine,G2Affine}::from_uncompressed`
    
    * update encoding docs to include curve equation
    
    * use const generic for generating pairing instances for bench
    
    * add link to the zcash spec
    
    * remove redundant `is_on_curve` check
    
    * update comments on decompress
    
    * make docs more readable
    
    * cargo fmt
    
    * add rountrip tests
    
    * add max pairing length
    
    * update the target group ordering
    
    * remove `func` fields in tests
    
    * give more specific test names
    
    * cargo lock

---
+++ bls12-381/src/test_vectors/mod.rs
@@ -0,0 +1,10 @@
+pub use {
+    addition::*, decompression::*, multiplication::*, pairing::*, subtraction::*, validation::*,
+};
+
+pub mod addition;
+pub mod decompression;
+pub mod multiplication;
+pub mod pairing;
+pub mod subtraction;
+pub mod validation;
