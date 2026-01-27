eda051677b19044fcf44857b72c8157cca3c6173
yihau <yihau.chen@icloud.com>
2026-01-27 23:49:27 +0800

    ci: remove duplicate check

--- ci/test-checks.sh
+++ ci/test-checks.sh
@@ -29,25 +29,6 @@ _ scripts/check-dev-context-only-utils.sh tree
 # fmt
 _ scripts/cargo-for-all-lock-files.sh -- "+${rust_nightly}" fmt --all -- --check
 
-# run cargo check for all rust files in this monorepo for faster turnaround in
-# case of any compilation/build error for nightly
-
-# Only force up-to-date lock files on edge
-if [[ $CI_BASE_BRANCH = "$EDGE_CHANNEL" ]]; then
-  if _ scripts/cargo-for-all-lock-files.sh "+${rust_nightly}" check \
-    --locked --workspace --all-targets --features dummy-for-ci-check,frozen-abi; then
-    true
-  else
-    check_status=$?
-    echo "$0: Some Cargo.lock might be outdated; sync them (or just be a compilation error?)" >&2
-    echo "$0: protip: $ ./scripts/cargo-for-all-lock-files.sh [--ignore-exit-code] ... \\" >&2
-    echo "$0:   [tree (for outdated Cargo.lock sync)|check (for compilation error)|update -p foo --precise x.y.z (for your Cargo.toml update)] ..." >&2
-    exit "$check_status"
-  fi
-else
-  echo "Note: cargo-for-all-lock-files.sh skipped because $CI_BASE_BRANCH != $EDGE_CHANNEL"
-fi
-
 _ ci/order-crates-for-publishing.py
 
 _ scripts/cargo-clippy.sh
